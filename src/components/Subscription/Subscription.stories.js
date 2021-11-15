9132d69558173942416b5e862cf29f3efd0cb829
anil verma <anilcs240@gmail.com>
2021-11-15 18:42:14 +0530

    stories inside stories

---
+++ src/components/Subscription/Subscription.stories.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Primary } from '../Button/Button.stories'
+export default {
+    title: 'Form/Subscription',
+}
+
+export const PrimarySubscription = () => <Primary />
