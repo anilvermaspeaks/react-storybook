830da4aaf15431eb47994f0db589e5da3a369e7b
anil verma <anilcs240@gmail.com>
2021-11-16 09:35:30 +0530

    added center decorator, it is like wrapping story

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Button from './Button';
-
+import Center from '../Center/Center'
 
 export default {
     title: 'Button',
     component: Button,
+    decorators: [story => <Center>{story()}</Center>]
     // args: {
     //     children: 'Button'
     // }
 }
 
-export const Primary = () => <Button variant='primary'>Primary</Button>
+
+
 export const Secondary = () => <Button variant='secondary'>Secondary</Button>
 export const Success = () => <Button variant='success'>Success</Button>
 export const Danger = () => <Button variant='danger'>Danger</Button>
